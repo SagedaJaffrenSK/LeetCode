48b1d265d001a1b7b4754564a8e6e15662299507
Sageda Jaffren Shaik <147475872+SagedaJaffrenSK@users.noreply.github.com>
2025-09-22 14:03:23 +0530

    Time: 12 ms (6.6%), Space: 5.9 MB (100%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,16 @@
+bool isPalindrome(int x) {
+    long int rev=0, temp=x;
+    while(temp > 0)
+    {
+        int rem = temp % 10;
+        rev = rev*10+rem;
+        temp /= 10;
+    }
+    if(x==rev)
+    {
+        return true;
+    }
+    else  {
+        return false;
+    }
+}
