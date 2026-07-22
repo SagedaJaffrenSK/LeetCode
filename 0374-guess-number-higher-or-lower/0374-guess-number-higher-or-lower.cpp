f24c35944ae4e1b7c35428ff00edd5c930a8a7a5
Shaik Sageda Jaffren <23a91a6152@aec.edu.in>
2026-07-22 11:07:48 +0530

    Time: 2 ms (55.71%), Space: 7.9 MB (75.27%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -10,13 +10,17 @@
 class Solution {
 public:
     int guessNumber(int n) {
-        long long int low=1, high=n;
-        long long int temp=n;
+        int low=1, high=n;
         while(low<=high){
-            long long int mid = (low+high)/2;
-            long long int res=guess(mid);
-            if(res==0){
+            int mid = low + (high-low) / 2;
+            if(guess(mid)==0){
                 return mid;
+            }
+            else if(guess(mid)==-1){
+                high=mid-1;
+            }
+            else if(guess(mid)==1){
+                low=mid+1;
             }
         }
         return -1;
