9843d77ca4876f291b1615b5c746dfbe0d4d178a
Shaik Sageda Jaffren <23a91a6152@aec.edu.in>
2026-08-03 14:08:26 +0530

    Time: 19 ms (96.46%), Space: 82.7 MB (89%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int n=nums.size();
-        int count=0;
-        for(int i=1; i<n; i++){
-            if(nums[i-1]==nums[i]){
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i]==nums[i+1]){
                 return true;
             }
         }
