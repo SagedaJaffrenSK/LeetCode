76a22811b8359f16d8ae837c8b13f4e6527f4025
Shaik Sageda Jaffren <23a91a6152@aec.edu.in>
2026-08-04 11:31:21 +0530

    Time: 0 ms (100%), Space: 29.7 MB (47.04%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-        int temp=nums[n-1];
         k = k%n;
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin()+k);
